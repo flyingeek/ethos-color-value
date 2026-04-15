a62c0b285f778ade69e3f5a19420e8cf94267e4b
Eric Delord <04-ete.elit@icloud.com>
2026-04-15 16:26:53 +0200

    feat: _x tag accepts negative value

--- color-value/lib/logics.lua
+++ color-value/lib/logics.lua
@@ -65,7 +65,7 @@ local function replaceTag(s, source)
                 function(multiplier)
                     return tostring(math.floor(0.5 + (tonumber(source:value()) or 0) * tonumber("1" .. multiplier)))
                 end)
-            :gsub("_([%d.]+)x", -- _%fx multiplier using source:decimals...
+            :gsub("_(-?[%d.]+)x", -- _%fx multiplier using source:decimals...
                 function(multiplier)
                     return tostring(formatWithDecimals((tonumber(source:value()) or 0) * (tonumber(multiplier) or 0),
                         source))
